278c5dc911df4adccaaaae72335acd65ee9e997a
uelola <80069286+uelola@users.noreply.github.com>
2026-05-15 00:34:19 +0200

    Reactive Forms approach

--- src/app/recipes/recipe-edit/recipe-edit.component.ts
+++ src/app/recipes/recipe-edit/recipe-edit.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute, Params } from '@angular/router';
+import { FormArray, FormControl, FormGroup, Validators } from '@angular/forms';
+import { ActivatedRoute, Params, Router } from '@angular/router';
+import { RecipeService } from '../recipe.service';
 
 @Component({
   selector: 'app-recipe-edit',
@@ -9,13 +11,99 @@ import { ActivatedRoute, Params } from '@angular/router';
 export class RecipeEditComponent implements OnInit {
   id: number | undefined;
   editMode = false;
+  recipeForm!: FormGroup;
 
-  constructor(private route: ActivatedRoute) {}
+  get controls() {
+    // a getter! needed for accessing the controls in the template
+    return (<FormArray>this.recipeForm.get('ingredients')).controls;
+    //return (this.recipeForm.get('ingredients') as FormArray).controls;
+  }
+
+  constructor(
+    private route: ActivatedRoute,
+    private recipeService: RecipeService,
+    private router: Router,
+  ) {}
 
   ngOnInit() {
     this.route.params.subscribe((params: Params) => {
       this.id = +params['id'];
+      // this.editMode = params['id'] != null;
       this.editMode = params['id'] !== undefined && !isNaN(+params['id']);
+      this.initForm();
+    });
+  }
+
+  onSubmit() {
+    // const newRecipe = new Recipe(
+    //   this.recipeForm.value['name'],
+    //   this.recipeForm.value['description'],
+    //   this.recipeForm.value['imagePath'],
+    //   this.recipeForm.value['ingredients'],
+    // );
+    if (this.editMode) {
+      this.recipeService.updateRecipe(this.id!, this.recipeForm.value);
+    } else {
+      this.recipeService.addRecipe(this.recipeForm.value);
+    }
+    this.onCancel();
+  }
+
+  onDeleteIngredient(index: number) {
+    (<FormArray>this.recipeForm.get('ingredients')).removeAt(index);
+  }
+
+  onCancel() {
+    this.router.navigate(['../'], { relativeTo: this.route });
+  }
+
+  onAddIngredient() {
+    (<FormArray>this.recipeForm.get('ingredients')).push(
+      new FormGroup({
+        name: new FormControl(null, Validators.required),
+        amount: new FormControl(null, [
+          Validators.required,
+          Validators.pattern(/^[1-9]+[0-9]*$/),
+        ]),
+      }),
+    );
+  }
+
+  // onDeleteIngredient(index: number) {
+  //   (<FormArray>this.recipeForm.get('ingredients')).removeAt(index);
+  // }
+
+  private initForm() {
+    let recipeName = '';
+    let recipeImagePath = '';
+    let recipeDescription = '';
+    let recipeIngredients = new FormArray<FormGroup>([]);
+
+    if (this.editMode) {
+      const recipe = this.recipeService.getRecipe(this.id!);
+      recipeName = recipe.name;
+      recipeImagePath = recipe.imagePath;
+      recipeDescription = recipe.description;
+      if (recipe['ingredients']) {
+        for (let ingredient of recipe.ingredients) {
+          recipeIngredients.push(
+            new FormGroup({
+              name: new FormControl(ingredient.name, Validators.required),
+              amount: new FormControl(ingredient.amount, [
+                Validators.required,
+                Validators.pattern(/^[1-9]+[0-9]*$/),
+              ]),
+            }),
+          );
+        }
+      }
+    }
+
+    this.recipeForm = new FormGroup({
+      name: new FormControl(recipeName, Validators.required),
+      imagePath: new FormControl(recipeImagePath, Validators.required),
+      description: new FormControl(recipeDescription, Validators.required),
+      ingredients: recipeIngredients,
     });
   }
 }
--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -2,9 +2,12 @@ import { Injectable } from '@angular/core';
 import { Recipe } from './recipe.model';
 import { Ingredient } from '../shared/ingredient.model';
 import { ShoppingListService } from '../shopping-list/shopping-list.service';
+import { Subject } from 'rxjs-compat';
 
 @Injectable()
 export class RecipeService {
+  recipesChanged = new Subject<Recipe[]>();
+
   private recipes: Recipe[] = [
     new Recipe(
       'Crispy Rice Paper Dumplings',
@@ -17,6 +20,12 @@ export class RecipeService {
       'Tasty keto meal prep for the week.',
       'https://www.shopflavcity.com/cdn/shop/articles/low-carb-chicken-stew-keto-cauliflower-rice-1.jpg?v=1759303140&width=720',
       [new Ingredient('Chicken', 300), new Ingredient('Cauliflower Rice', 100)],
+    ),
+    new Recipe(
+      'Lamb Tagine',
+      'Lamb Tagine, rich flavours in the sauce.',
+      'https://www.shemins.com/wp-content/uploads/2022/09/Lamb-and-Apricot-Tagine-537x537.jpg',
+      [new Ingredient('Lamb', 500), new Ingredient('Apricots', 50)],
     ),
   ];
 
@@ -32,5 +41,19 @@ export class RecipeService {
 
   addIngredientsToShoppingList(ingredients: Ingredient[]) {
     this.slService.addIngredients(ingredients);
+  }
+  addRecipe(recipe: Recipe) {
+    this.recipes.push(recipe);
+    this.recipesChanged.next(this.recipes.slice());
+  }
+
+  updateRecipe(index: number, newRecipe: Recipe) {
+    this.recipes[index] = newRecipe;
+    this.recipesChanged.next(this.recipes.slice());
+  }
+
+  deleteRecipe(index: number) {
+    this.recipes.splice(index, 1);
+    this.recipesChanged.next(this.recipes.slice());
   }
 }
